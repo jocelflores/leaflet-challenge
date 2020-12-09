2ae74afc8be9ee9c48b7b30ac2e6e9ce9fc1e588
jocelflores <jocelynflores@utexas.edu>
2020-12-08 23:06:00 -0600

    images

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,12 @@
 var myMap = L.map("mapid").setView([38.809,-98.5556199], 5);
 
-    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "streets-v11",
-        accessToken: "pk.eyJ1Ijoiam9jZWxmbG9yZXMiLCJhIjoiY2tocGl4ZmZ4MDJvMjMzbzV6MGM5NDg0NSJ9.Q6cT_e1lI1u4yeJUGm6O4Q"
-    }).addTo(myMap);
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: "pk.eyJ1Ijoiam9jZWxmbG9yZXMiLCJhIjoiY2tocGl4ZmZ4MDJvMjMzbzV6MGM5NDg0NSJ9.Q6cT_e1lI1u4yeJUGm6O4Q"
+}).addTo(myMap);
+
+    
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 //response.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=lax")
